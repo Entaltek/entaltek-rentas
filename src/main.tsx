847bbb7351b29f5b70b42a9b32bdd6776ebaa378
Carlos Cano <66888650+carloscano-PyRep@users.noreply.github.com>
2026-06-28 21:31:19 -0600

    feat: route public property links

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HomePage } from './pages/HomePage';
+import { PublicPropertyPage } from './pages/PublicPropertyPage';
 import './styles.css';
+
+function App() {
+  const path = window.location.pathname;
+  const publicPropertyMatch = path.match(/^\/r\/(.+)$/);
+
+  if (publicPropertyMatch?.[1]) {
+    return <PublicPropertyPage slug={decodeURIComponent(publicPropertyMatch[1])} />;
+  }
+
+  return <HomePage />;
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <HomePage />
+    <App />
   </React.StrictMode>
 );
